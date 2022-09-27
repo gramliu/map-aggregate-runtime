4363a2aca5afa69c2357cd0310d7c1ae81f331eb
Gram Liu <gram.liu226@gmail.com>
2022-09-27 19:50:00 -0400

    Add Choose node

---
+++ nodes/Choose.ts
@@ -0,0 +1,27 @@
+import { MapAggregateNode, Node } from "../core/Node";
+import Payload from "../core/Payload";
+import Schema from "../core/Schema";
+
+type ChooseProps = {
+  count: number;
+};
+
+@MapAggregateNode("Choose", "Choose the first k payloads")
+export default class Choose extends Node<ChooseProps> {
+  async process(input: Payload[]): Promise<Payload[]> {
+    if (input.length < this.params.count) {
+      return input;
+    } else {
+      return input.slice(0, this.params.count);
+    }
+  }
+
+  getSchema(): Schema<ChooseProps> {
+    return {
+      count: {
+        description:
+          "The number of payloads to select. If there are less payloads, all are sent",
+      },
+    };
+  }
+}
